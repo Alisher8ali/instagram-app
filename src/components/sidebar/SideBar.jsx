e1a57bb7ccac9774c3d98fb4e38d66b379f1a17f
Alisher8ali <alisher95211@gmail.com>
2023-02-17 02:13:33 +0600

    Create sidebar and layout

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import cls from "../../assets/styles/sidebar/SideBar.module.scss"
+import { Components } from '..';
+import cls from "../../assets/styles/sidebar/SideBar.module.scss";
+import Logo from "../../assets/images/logo/instagram.png"
 
 export const SideBar = () => {
   return (
-    <section className=''>
-        SideBar
+    <section className={cls.sidebar_wrapper}>
+        <div className={cls.sidebar_wrapper_content}>
+            <Components.Image src={Logo}/>
+        </div>
     </section>
   )
 };
